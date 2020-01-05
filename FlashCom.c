9df614ec57e5898258f683602a362eaf44b3a174
Shabtronic <58636376+shabtronic@users.noreply.github.com>
2020-01-05 16:42:11 +0000

    now fully working!
    no longer uses timer interrupt - since that simply won't work - that hangs the uart interrupt.
    
    have to call
    extern void SerialHandler();
    SerialHandler();
    
    in kernel.cpp - somewhere in a update loop for the moment.

--- FlashCom.c
+++ FlashCom.c
@@ -1,43 +1,87 @@
-/*
- * converttool.c
- *
- * Converts a web content file to a C string or array (with -b).
- */
+
 #include <windows.h>
+//#include <timeapi.h>
 #include <stdio.h>
 #include <string.h>
+const int UartSpeed = 115200 * 8;
 HANDLE     hStdout;
+#define ConsumeTimeout 5
+#define BLUE  1
+#define GREEN 2
+#define CYAN 3
+#define RED 4
+#define PURPLE 5
+#define YELLOW 6
+#define WHITE 7
+#define ORIGIN "PC: "
+
+unsigned int crc32_for_byte(unsigned int r)
+	{
+	for (int j = 0; j < 8; j++)
+		r = (r & 1 ? 0 : (unsigned int)0xEDB88320L) ^ r >> 1;
+	return r ^ (unsigned int)0xFF000000L;
+	}
+
+unsigned int crc32(const void *data, size_t n_bytes)
+	{
+	unsigned int crc = 0;
+	static unsigned int table[0x100] = { 0 };
+	if (!*table)
+		for (int i = 0; i < 0x100; i++)
+			table[i] = crc32_for_byte(i);
+	for (size_t i = 0; i < n_bytes; ++i)
+		crc = table[(unsigned char)crc ^ ((unsigned char*)data)[i]] ^ crc >> 8;
+	return crc;
+	}
+
+void TextColor(int col)
+	{
+	SetConsoleTextAttribute(hStdout, col | FOREGROUND_INTENSITY);
+	}
 
 HANDLE OpenPort(char* Name)
 	{
 
 	//	system("mode com1: baud=9600 parity=n data=8 stop=1 to=off xon=off");
 	HANDLE hSerial;
-	hSerial = CreateFile(Name,	GENERIC_READ | GENERIC_WRITE,	0,	0,	OPEN_EXISTING,	FILE_ATTRIBUTE_NORMAL,	0);
+	hSerial = CreateFile(Name, GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
 	if (hSerial == INVALID_HANDLE_VALUE)
 		{
 		if (GetLastError() == ERROR_FILE_NOT_FOUND)
 			{
 			//serial port does not exist. Inform user.
 			}
-		fprintf(stderr, "\nError Opening Serial Port\n");
-		SetConsoleTextAttribute(hStdout, 7 | FOREGROUND_INTENSITY);
-
+		TextColor(RED);
+		fprintf(stderr, "Error opening: %s\n", Name);
+		TextColor(WHITE);
+		return 0;
 
 		//some other error occurred. Inform user.
 		}
+	return hSerial;
 	}
+
+int FileExists(char* filename)
+	{
+	FILE *F;
+	F = fopen(filename, "rb");
+	if (F)
+		{
+		fclose(F);
+		return 1;
+		}
+	return 0;
+	}
+
 void PortConfig(HANDLE port, int Speed)
 	{
 	DCB dcbSerialParams = { 0 };
 	dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
 	if (!GetCommState(port, &dcbSerialParams))
 		{
-		SetConsoleTextAttribute(hStdout, FOREGROUND_RED | FOREGROUND_INTENSITY);
-
-		fprintf(stderr, "\nError Getting Com State\n");
-		SetConsoleTextAttribute(hStdout, 7 | FOREGROUND_INTENSITY);
-
+		TextColor(RED);
+		//fprintf(stderr, "Error Getting Com State\n");
+		TextColor(WHITE);
 		//error getting state
 		}
 	dcbSerialParams.BaudRate = (DWORD)Speed;
@@ -46,11 +90,9 @@ void PortConfig(HANDLE port, int Speed)
 	dcbSerialParams.Parity = NOPARITY;
 	if (!SetCommState(port, &dcbSerialParams))
 		{
-		SetConsoleTextAttribute(hStdout, FOREGROUND_RED | FOREGROUND_INTENSITY);
-
-		fprintf(stderr, "\nError Setting Com State\n");
-		SetConsoleTextAttribute(hStdout, 7 | FOREGROUND_INTENSITY);
-
+		TextColor(RED);
+		//fprintf(stderr, "Error Setting Com State\n");
+		TextColor(WHITE);
 		//error setting serial port state
 		}
 
@@ -63,17 +105,22 @@ void PortConfig(HANDLE port, int Speed)
 	timeouts.WriteTotalTimeoutMultiplier = 10;
 	if (!SetCommTimeouts(port, &timeouts))
 		{
-		SetConsoleTextAttribute(hStdout, FOREGROUND_RED | FOREGROUND_INTENSITY);
-
-		fprintf(stderr, "\nError Setting Timeouts\n");
-		SetConsoleTextAttribute(hStdout, 7 | FOREGROUND_INTENSITY);
-
-		//error occureed. Inform user
+		TextColor(RED);
+		//fprintf(stderr, "Error Setting Timeouts\n");
+		TextColor(WHITE);
 		}
 	}
 void ClosePort(HANDLE port)
 	{
 
+	}
+
+double GetTime()
+	{
+	__int64 time1 = 0, time2 = 0, freq = 0;
+	QueryPerformanceCounter((LARGE_INTEGER *)&time1);
+	QueryPerformanceFrequency((LARGE_INTEGER *)&freq);
+	return (double)time1 / freq;
 	}
 void uSleep(int waitTime)
 	{
@@ -85,64 +132,205 @@ void uSleep(int waitTime)
 		QueryPerformanceCounter((LARGE_INTEGER *)&time2);
 		} while ((time2 - time1) < waitTime);
 	}
-void Write(HANDLE cp,char* m)
+void Write(HANDLE cp, char* m)
 	{
 	DWORD BytesWritten;
 	WriteFile(cp, m, strlen(m), &BytesWritten, NULL);
-
 	}
+void WriteBytes(HANDLE cp, char* buf, int size)
+	{
+	DWORD BytesWritten;
+	WriteFile(cp, buf, size, &BytesWritten, NULL);
+	}
+
+
+int Consume(HANDLE ComPort, char* EatMe, int TimeOut)
+	{
+	char* teatme = EatMe;
+	int count = 0;
+	while (*teatme && count < TimeOut)
+		{
+		char Byte;
+		DWORD dwBytesRead;
+		ReadFile(ComPort, &Byte, 1, &dwBytesRead, NULL);
+		if (dwBytesRead == 1)
+			{
+			if (Byte == *teatme)
+				teatme++;
+			else
+				teatme = EatMe;
+			}
+		else
+			count++;
+		}
+	if (*teatme)
+		return 0;
+	return 1;
+	}
+int SendFile(HANDLE comport, char* filename)
+	{
+	int res = 1;
+	printf("PC: Sending File %s to PI\n", filename);
+	FILE *F;
+	F = fopen(filename, "rb");
+	if (F)
+		{
+		double st = GetTime();
+		fseek(F, 0, SEEK_END);
+		int fsize = ftell(F);
+		fseek(F, 0, SEEK_SET);
+		char* Data = (char*)malloc(fsize);
+		fread(Data, 1, fsize, F);
+		printf("PC: file size is %2.1fKb\n", (float)fsize / (1024));
+		unsigned int crc = crc32(Data, fsize);
+		//printf("file crc is %u\n", crc);
+		char sstring[1024];
+		sprintf(sstring, "PICMD:FILE%s,%d,%d,%u,RAW,", filename, fsize, fsize, crc);
+		Write(comport, sstring);
+		char *tdata = Data;
+		int chunk = 0;
+		int ofsize = fsize;
+		printf("PC: File Transfer: ");
+		while (fsize)
+			{
+			float percent = 100 - 100 * ((float)fsize / ofsize);
+			TextColor(GREEN);
+			printf("%5.1f%%\b\b\b\b\b\b", percent);
+			TextColor(WHITE);
+			int BytesWritten = 0;
+			if (fsize > 1024)
+				{
+				WriteFile(comport, tdata, 1024, &BytesWritten, NULL);
+				tdata += 1024;
+				fsize -= 1024;
+				if (BytesWritten != 1024 || !Consume(comport, "FOK", 50))
+					{
+					if (BytesWritten != 1024)
+						printf("PC: Error Sending File Chunk %d Bytes Written %d\n", chunk, BytesWritten);
+					else
+						printf("PC: Error Sending File Chunk %d no FOK received\n", chunk, BytesWritten);
+					res = 0;
+					goto cleanup;
+					}
+				}
+			else
+				{
+				WriteFile(comport, tdata, fsize, &BytesWritten, NULL);
+				if (BytesWritten != fsize || !Consume(comport, "FOK", 500))
+					{
+					if (BytesWritten != fsize)
+						printf("PC: Error Sending File Chunk %d Bytes Written %d\n", chunk, BytesWritten);
+					else
+						printf("PC: Error Sending File Chunk %d no FOK received\n", chunk, BytesWritten);
+
+					res = 0;
+					goto cleanup;
+					}
+				fsize = 0;
+				TextColor(GREEN);
+				st = GetTime() - st;
+				printf("100%%  in %2.1f secs\n\n", st);
+				TextColor(WHITE);
+
+				}
+			chunk++;
+			}
+		//	printf("\n");
+	cleanup:
+		fclose(F);
+		free(Data);
+		}
+	else
+		printf("PC: error file %s not found\n", filename);
+	return res;
+	}
+HANDLE EnumComPorts()
+	{
+	//	printf("Enum Com Ports:\n\n");
+	char Res[1024];
+	static char ComString[1024];
+	for (int a = 0; a < 50; a++)
+		{
+		Res[0] = 0;
+
+		sprintf(ComString, "COM%d", a);
+		QueryDosDevice(ComString, Res, 1024);
+		if (Res[0] != 0)
+			{
+			//printf("%s %s\n", ComString, Res);
+			HANDLE ComPort = OpenPort(ComString);
+			PortConfig(ComPort, UartSpeed);
+			Write(ComPort, "PICMD:HELO");
+			if (Consume(ComPort, "PI: We are Connected!", 5))
+				{
+				//	TextColor(YELLOW);
+				printf("PC: Found PI on %s %s\n", ComString, Res);
+				//	TextColor(WHITE);
+				return ComPort;
+				}
+			ClosePort(ComPort);
+			}
+		}
+	printf("\n");
+	return 0;
+	}
+
 int main(int nArgC, char **ppArgV)
 	{
 	hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
-	HANDLE ComPort = OpenPort("COM7");
-	//HANDLE ComPort = OpenPort(ppArgV[1]);
-	PortConfig(ComPort, 115200);
+	system("cls");
+	TextColor(GREEN);
+	printf("FlashCom V1.00 Serial PI Comms Utility (C) 2020 S.D.Smith\n\n");
+	TextColor(WHITE);
+	//EnumComPorts();
+
+	HANDLE ComPort = EnumComPorts();
+	if (!ComPort)
+		{
+		printf("PC: PI not found :(\n");
+		return 0;
+		}
+	//PortConfig(ComPort, UartSpeed);
+
+//	TextColor(BLUE);
+	fprintf(stderr, "PC: Uart speed is %d * 115200 = %2.1fKb/s\n", 8, (float)UartSpeed / (10 * 1024));
+	//	TextColor(WHITE);
+
 	char *pArg0 = *ppArgV++;
 	nArgC--;
-
-	system("cls");
-	SetConsoleTextAttribute(hStdout, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-	printf("FlashCom V1.00 Serial Pi Comms Transfer Utility (C) 2020 S.D.Smith\n\n", pArg0);
-	SetConsoleTextAttribute(hStdout, 7 | FOREGROUND_INTENSITY);
-
-	if (nArgC != 2)
+	char* filesend = 0;
+	int Reboot = 0;
+	for (int a = 0; a < nArgC; a++)
 		{
-		SetConsoleTextAttribute(hStdout, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
-		fprintf(stderr, "Usage: FlashCom PortName[COM7] Speed[115200] [FileName] [Compress]\n\n", pArg0);
-		SetConsoleTextAttribute(hStdout, 7 | FOREGROUND_INTENSITY);
-		}
-	SetConsoleTextAttribute(hStdout, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-	printf("Port Com7 Open and waiting:\n\n", pArg0);
-	SetConsoleTextAttribute(hStdout, 7 | FOREGROUND_INTENSITY);
-	// Send Hello
-	Write(ComPort, "PICMD:HELO");
+		if (FileExists(ppArgV[a]))
+			filesend = ppArgV[a];
 
-	//Sleep(1000);
-	int cc = 0;
+		if (strstr(ppArgV[a], "reboot") > 0)
+			Reboot = 1;
+		}
+
+
+	if (filesend)
+		{
+		if (SendFile(ComPort, filesend))
+			Sleep(400); // sleep a little so PI has a chance to come out of handler and write file
+		}
+	if (Reboot)
+		{
+		printf("PC: Rebooting PI\n");
+		Write(ComPort, "PICMD:RBOO");
+		}
 
 	while (1)
 		{
 		char szBuff[16] = { 0 };
 		DWORD dwBytesRead = 0;
 		ReadFile(ComPort, szBuff, 1, &dwBytesRead, NULL);
+		//TextColor(YELLOW);
 		if (dwBytesRead == 1)
-			{
 			printf("%c", szBuff[0]);
-			}
+		//TextColor(WHITE);
 		uSleep(1);
-		cc++;
-		if (cc == 500)
-			{
-			printf("Sending File:\n");
-			Write(ComPort, "PICMD:FILE");
-			//Write(ComPort, "Kernel.img,12344,123454589,");
-			}
-		if (cc == 1000)
-			{
-			printf("Rebooting PI:\n");
-			Write(ComPort, "PICMD:RBOO");
-			}
-
 		}
 	ClosePort(ComPort);
 	return 0;
--- main.cpp
+++ main.cpp
@@ -19,6 +19,29 @@
 //
 #include "kernel.h"
 #include <circle/startup.h>
+#define ANSIRED "\033[0;31m"
+#define ANSIGREEN "\033[0;32m"
+#define ANSIWHITE "\033[0m"
+extern int atoi(char *str);
+
+unsigned int crc32_for_byte(unsigned int r)
+	{
+	for (int j = 0; j < 8; j++)
+		r = (r & 1 ? 0 : (unsigned int)0xEDB88320L) ^ r >> 1;
+	return r ^ (unsigned int)0xFF000000L;
+	}
+
+unsigned int crc32(const void *data, size_t n_bytes)
+	{
+	unsigned int crc = 0;
+	static unsigned int table[0x100] = { 0 };
+	if (!*table)
+		for (int i = 0; i < 0x100; i++)
+			table[i] = crc32_for_byte(i);
+	for (size_t i = 0; i < n_bytes; ++i)
+		crc = table[(unsigned char)crc ^ ((unsigned char*)data)[i]] ^ crc >> 8;
+	return crc;
+	}
 
 CKernel Kernel;
 int I2CWrite(int addr, unsigned char* buf, int len)
@@ -26,9 +49,6 @@ int I2CWrite(int addr, unsigned char* buf, int len)
 	return Kernel.m_I2CMaster.Write(addr, buf, len);
 	}
 
-// FileSize00000000
-// CheckSum00000000
-// FileData000000000000000000
 void WaitRec(char* buf, int size, bool AddNull = true)
 	{
 	while (size)
@@ -40,39 +60,164 @@ void WaitRec(char* buf, int size, bool AddNull = true)
 	if (AddNull)
 		*buf = 0;
 	}
-void WaitRecDelim(char* buf, char delimiter, bool AddNull = true)
+CString WaitRecDelim(char delimiter)
 	{
-	char lastchar = 0;
-	while (lastchar=delimiter)
-		{ }
-		if (Kernel.m_Serial.Read(&lastchar, 1) > 0)
+	CString str = "";
+	char lastchar[2] = { 0, 0 };
+	while (*lastchar != delimiter)
+		{
+		if (Kernel.m_Serial.Read(lastchar, 1) == 1)
 			{
-			if (lastchar!=delimiter)
-			*buf++= lastchar;
+			lastchar[1] = 0;
+			if (*lastchar != delimiter)
+				str.Append(lastchar);
 			}
-	if (AddNull)
-		*buf = 0;
-	}
-void TimerHandler(TKernelTimerHandle hTimer, void *pParam, void *pContext)
-	{
-	char sbuf[1024];
-	//Get Magic String
-	WaitRec(sbuf, 6);
-	// Get command
-	WaitRec(sbuf, 4);
-	Kernel.log.printf("PI: Cmd-%s\n", sbuf);
-	if (strstr(sbuf, "HELO"))		
-		Kernel.log.printf("PI: We are Connected!\n");
-	if (strstr(sbuf, "FILE"))
-		{
-		CString filename = "";
 		}
-	if (strstr(sbuf, "RBOO")) // Reboot the PI!
+	return str;
+	}
+void EmptyFifo()
+	{
+	char lastchar[2];
+	while (Kernel.m_Serial.Read(lastchar, 1) == 1);
+	}
+void Consume(char *eatme)
+	{
+	char* teatme = eatme;
+	char byte;
+	while (*teatme)
 		{
-		reboot();
+		if (Kernel.m_Serial.Read(&byte, 1) == 1)
+			if (byte == *teatme)
+				teatme++;
+			else
+				teatme = eatme;
 		}
 	}
 
+bool ConsumeBytes(char* data, int count)
+	{
+	int timeout = 0;
+	int cc = 0;
+	while (count > 0)
+		{
+		if (Kernel.m_Serial.Read(data, 1) == 1)
+			{
+			data++;
+			count--;
+			}
+		cc++;
+		if ((cc & 0xffffff) == 0xffffff)
+			{
+			Kernel.m_Serial.printf("Stuck in ConsumeBytes! %d Count: %d\n", cc, count);
+			Kernel.m_Serial.Flush();
+			}
+		}
+	if (count == 0) return true;
+	return false;
+	}
+static volatile bool TimeRun = false;
+static volatile int TimeInst = 0;
+//void TimerHandler(TKernelTimerHandle hTimer, void *pParam, void *pContext)
+static volatile bool SerialProcess = false;
+void SerialHandler()
+	{
+	if (!SerialProcess) return;
+	//	Kernel.m_Serial.printf("Timer Handler\n");
+	//	return;
+	if (TimeRun) return;
+	TimeRun = true;
+	TimeInst++;
+	char sbuf[1024];
+	// Eat the Magic String
+//	WaitRec(sbuf, 6);
+	Consume("PICMD:");
+	// Get command
+	WaitRec(sbuf, 4);
+	if (strstr(sbuf, "HELO") > 0)
+		Kernel.m_Serial.printf("PI: We are Connected!\n");
+	if (strstr(sbuf, "FILE") > 0)
+		{
+		CString filename = WaitRecDelim(',');
+		CString filesize = WaitRecDelim(',');
+		CString filesize2 = WaitRecDelim(',');
+		CString filecrc32 = WaitRecDelim(',');
+		CString filetype = WaitRecDelim(',');
+		int fsize = atoi(filesize.c_str());
+		int ofsize = fsize;
+		unsigned int crc = atoi(filecrc32.c_str());
+		char* filedata = (char*)malloc(fsize);
+		char* tfiledata = filedata;
+		int chunk = 0;
+		while (fsize > 0)
+			{
+			if (fsize > 1024)
+				{
+				if (ConsumeBytes(tfiledata, 1024))
+					{
+					tfiledata += 1024;
+					fsize -= 1024;
+					Kernel.m_Serial.printf("FOK");
+					}
+				else
+					{
+					Kernel.m_Serial.printf("PI: Error receiveing file.\n");
+					fsize = 0;
+					}
+				}
+			else
+				{
+				if (ConsumeBytes(tfiledata, fsize))
+					{
+					fsize = 0;
+					Kernel.log.printf("FOK");
+					}
+				else
+					{
+					Kernel.m_Serial.printf("PI: Error receiveing file.\n");
+					fsize = 0;
+					}
+
+				}
+			chunk++;
+			}
+		unsigned int freshcrc = crc32(filedata, ofsize);
+		Kernel.m_Serial.printf("PI: file data received OK!\n");
+		Kernel.m_Serial.printf("PI: sent crc %u my crc %u\n", crc, freshcrc);
+
+		if (crc == freshcrc)
+			{
+			Kernel.m_Serial.printf("PI:"ANSIGREEN" Success crc's match - writing file"ANSIWHITE"\n");
+			FIL File;
+			f_open(&File, filename.c_str(), FA_WRITE | FA_CREATE_ALWAYS);
+			f_write(&File, filedata, ofsize, 0);
+			f_close(&File);
+			}
+		else
+			{
+			Kernel.m_Serial.printf("PI:"ANSIRED" Error crc's dont match!"ANSIWHITE"\n");
+			Kernel.m_Serial.printf("PI:"ANSIRED" writing as badfile.bad"ANSIWHITE"\n");
+			FIL File;
+			f_open(&File, "Badfile.bad", FA_WRITE | FA_WRITE| FA_CREATE_NEW);
+			f_write(&File, filedata, ofsize, 0);
+			f_close(&File);
+
+			}
+		if (filedata)
+			free(filedata);
+		}
+	if (strstr(sbuf, "RBOO") > 0) // Reboot the PI!
+		{
+		Kernel.m_Serial.printf("PI: Rebooting!\n");
+		Kernel.m_Serial.Flush();
+		reboot();
+		}
+	EmptyFifo();
+	TimeInst--;
+	TimeRun = false;
+	SerialProcess = false;
+	}
+
+// MagicHandler()
 // this is called from inside the kernel serial interrupt
 // function, so we need to kick off a timer function to
 // be able to read/write serial data without blocking
@@ -80,23 +225,19 @@ void TimerHandler(TKernelTimerHandle hTimer, void *pParam, void *pContext)
 
 void MagicHandler()
 	{
-	Kernel.m_Timer.StartKernelTimer(1, TimerHandler);	
+	SerialProcess = true;
+	//	Kernel.m_Timer.StartKernelTimer(30, TimerHandler);
 	}
 
 int main(void)
 	{
-
-
 	if (!Kernel.Initialize())
 		{
 		halt();;
 		return EXIT_HALT;
 		}
-	//	Kernel.m_Serial.RegisterMagicReceivedHandler("KernelFile:", SerialRec);
-	
-
-
 	Kernel.m_Serial.RegisterMagicReceivedHandler("PICMD:", MagicHandler);
+	//	Kernel.m_Timer.StartKernelTimer(30, TimerHandler);
 
 	TShutdownMode ShutdownMode = Kernel.Run();
 
